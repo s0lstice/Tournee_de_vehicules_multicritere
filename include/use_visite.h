888bf00675dac4788f88923948ef85d61fb33c19
Mickael PURET <puret_mickael@yahoo.fr>
2011-05-26 14:54:57 +0200

    mise à jour manuelle

--- include/use_visite.h
+++
@@ -1,8 +0,0 @@
-#ifndef USE_VISITE_H_INCLUDED
-#define USE_VISITE_H_INCLUDED
-
-int nb_visite_lieu(Donnee * data, int id_lieu_depart, int id_lieu_destination);
-void maj_nb_visite_lieu(Donnee * data, int id_lieu_depart, int id_lieu_destination, int nb_visite);
-void unall_visite(Donnee *data);
-
-#endif // USE_VISITE_H_INCLUDED
